9eb44b8cd4f0cbf2060ee2e962dc665093c73dc4
David Nunes <51808846+Dnuns@users.noreply.github.com>
2022-03-10 20:20:51 -0100

    Componentes Integráveis

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
-import './App.css';
-import { Component } from 'react';
+import "./App.css";
+import { Children, Component } from "react";
 
-const header = "Authors"
+const header = "Authors";
 
 const list = [
   {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
+    title: "React",
+    url: "https://facebook.github.io/react/",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
     objectID: 0,
   },
   {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
+    title: "Redux",
+    url: "https://github.com/reactjs/redux",
+    author: "Dan Abramov, Andrew Clark",
     num_comments: 2,
     points: 5,
     objectID: 1,
   },
+  {
+    title: "Nixt",
+    url: "https://github.com/reactjs/nixt",
+    author: "David Nunes, Academia de Código",
+    num_comments: 2,
+    points: 5,
+    objectID: 2,
+  }
 ];
 
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase()
-  .includes(searchTerm.toLowerCase());
+const isSearched = (searchTerm) => (item) =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       list,
-      searchTerm: ''
+      searchTerm: "",
     };
 
     this.onDismiss = this.onDismiss.bind(this);
@@ -41,8 +47,8 @@ class App extends Component {
   }
 
   onDismiss(id) {
-    const updatedList = this.state.list.filter(item => item.objectID !== id);
-    this.setState({ list: updatedList })
+    const updatedList = this.state.list.filter((item) => item.objectID !== id);
+    this.setState({ list: updatedList });
   }
 
   onSearchChange(event) {
@@ -50,96 +56,62 @@ class App extends Component {
   }
 
   render() {
+    const { searchTerm, list } = this.state;
 
-      const {searchTerm, list} = this.state;
-      
-      return (
-      
+    return (
       <div className="App">
         <Search 
           value={searchTerm} 
           onChange={this.onSearchChange}
-        />
+        >
+          Search
+        </Search>
         <Table 
-          list={list}
-          patern={searchTerm}
-          onDismiss={this.onDismiss}
+          list={list} 
+          patern={searchTerm} 
+          onDismiss={this.onDismiss} 
         />
-        <form>
-          <input 
-            type="text"
-            value={searchTerm}
-            onChange={this.onSearchChange}
-          />
-        </form>
-
-        {list.filter(isSearched(searchTerm)).map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-
-            <span>{item.author}</span>
-
-            <span>{item.num_comments}</span>
-
-            <span>{item.points}</span>
-
-            <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type='button'
-              >
-                Dismiss
-              </button>
-            </span>
-          </div>
-        )}
       </div>
-
     );
   }
 }
 
 class Search extends Component {
-  render(){
-    const {value, onChange} = this.props;
+  render() {
+    const { value, onChange, children } = this.props;
     return (
       <form>
-        <input
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
+        {children} <input 
+          type="text" 
+          value={value} 
+          onChange={onChange} 
+      />
       </form>
     );
   }
 }
 
 class Table extends Component {
-  render(){
-    const {list, patern, onDismiss} = this.props;
+  render() {
+    const { list, patern, onDismiss } = this.props;
 
-    return(
+    return (
       <div>
-        {list.filter(isSearched(patern)).map(item =>
+        {list.filter(isSearched(patern)).map((item) => (
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
             </span>
-            <span>{item.author}</span>          
-            <span>{item.num_comments}</span>          
-            <span>{item.points}</span>          
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
             <span>
-              <button
-                onClick={()=> onDismiss(item.objectID)}
-                type="button"
-              >
+              <button onClick={() => onDismiss(item.objectID)} type="button">
                 Dismiss
               </button>
             </span>
-          </div>          
-          )}
+          </div>
+        ))}
       </div>
     );
   }
